ad9d0ae71a73effae6f8e5aaf7e04edd8146f508
Kirill Pantsiuk <64860677+kirillpantsiuk@users.noreply.github.com>
2025-04-05 12:05:13 +0300

    05.04.25

--- Webapp_construction company/mern-auth-app/client/src/store/actions/actionTypes.js
+++
@@ -1,3 +0,0 @@
-// src/store/actions/actionTypes.js
-export const AUTH_SUCCESS = 'AUTH_SUCCESS';
-export const AUTH_LOGOUT = 'AUTH_LOGOUT';
--- Webapp_construction company/mern-auth-app/server/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { register, login, verifyToken } = require('../controllers/authController');
-const router = express.Router();
-
-router.post('/register', register);
-router.post('/login', login);
-router.post('/verify', verifyToken);
-
-module.exports = router;
